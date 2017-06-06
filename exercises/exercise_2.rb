45d3fe3e108b5c0f93dae177324d5443399bb61f
sevenveils <hghamilton@gmail.com>
2017-06-06 20:11:31 +0000

    Updated store names to store1, store2, store3 in the stores database

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,13 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+
+@store1 = Store.find(1)
+@store1.update(name: "store1")
+
+@store2 = Store.find(2)
+@store2.update(name: "store2")
+
+@store3 = Store.find(3)
+@store3.update(name: "store3")
+
