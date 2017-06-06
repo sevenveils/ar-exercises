ecc1f7a1917451db4a7d267981182374b7e20e1f
sevenveils <hghamilton@gmail.com>
2017-06-06 22:14:34 +0000

    syntax edited for consistency, throws error messages with employee or store is attempted to be added to the database without required fields

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,11 +10,11 @@ puts "----------"
 
 # Store1
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Heather", last_name: "Hamilton", hourly_rate: 100000)
+@store1.employees.create(first_name: "Heather", last_name: "Hamilton", hourly_rate: 100)
 @store1.employees.create(first_name: "Alysia", last_name: "Lynn", hourly_rate: 60)
 
 # Store2
 @store2.employees.create(first_name: "Bob", last_name: "Bobra", hourly_rate: 20)
-@store2.employees.create(first_name: "Frank", last_name: "Ocean", hourly_rate: 6000000)
-@store2.employees.create(first_name: "Solange", last_name: "Knowles", hourly_rate: 6000000)
+@store2.employees.create(first_name: "Frank", last_name: "Ocean", hourly_rate: 60)
+@store2.employees.create(first_name: "Solange", last_name: "Knowles", hourly_rate: 30)
 
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,18 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
+  validates :store_id, presence: true
+end
+
+p.errors.messages
+
+class Store < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+end
+
+p.errors.messages
