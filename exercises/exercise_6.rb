d3a18ffc521454d333467b3b73eb1543218417f4
sevenveils <hghamilton@gmail.com>
2017-06-06 21:23:47 +0000

    Employees database has employees, has_many relationship added to store.rb and belongs_to store to employees.rb

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# Store1
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Heather", last_name: "Hamilton", hourly_rate: 100000)
+@store1.employees.create(first_name: "Alysia", last_name: "Lynn", hourly_rate: 60)
+
+# Store2
+@store2.employees.create(first_name: "Bob", last_name: "Bobra", hourly_rate: 20)
+@store2.employees.create(first_name: "Frank", last_name: "Ocean", hourly_rate: 6000000)
+@store2.employees.create(first_name: "Solange", last_name: "Knowles", hourly_rate: 6000000)
+
